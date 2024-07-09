9baa0020beebf3de6f2f40f2acfd79d3d5c39050
miavai649 <mahmudulhaquenoor@gmail.com>
2024-07-09 15:04:50 +0600

    get all product controller created

--- src/app/modules/products/product.controller.ts
+++ src/app/modules/products/product.controller.ts
@@ -1,21 +1,33 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import { ProductServices } from "./product.service";
-import catchAsync from "../../utils/catchAsync";
-import sendResponse from "../../utils/sendResponse";
+import { ProductServices } from './product.service'
+import catchAsync from '../../utils/catchAsync'
+import sendResponse from '../../utils/sendResponse'
 
 const createProduct = catchAsync(async (req, res) => {
-  const productData = req.body;
+  const productData = req.body
 
-  const result = await ProductServices.createProductIntoDb(productData);
+  const result = await ProductServices.createProductIntoDb(productData)
   sendResponse(res, {
     statusCode: 200,
     success: true,
-    message: "Product created successfully",
-    data: result,
-  });
-});
+    message: 'Product created successfully',
+    data: result
+  })
+})
+
+const getAllProduct = catchAsync(async (req, res) => {
+  const result = await ProductServices.getAllProductFromDb()
+
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: 'Products fetched successfully',
+    data: result
+  })
+})
 
 export const ProductControllers = {
   createProduct,
-};
+  getAllProduct
+}
